0adce3b95814c2ec4a843c3f99c13481ab9fc61d
Robert Warner <robertfrederickwarner@gmail.com>
2015-02-17 11:51:29 +0200

    removed the hashbang from top of file. instructions for use involve setting up a virtualenv and calling explicitly using "python edrg.py". the file does not have the executable permission and even if it did, using it would use the system python and not the virtualenv python, resulting in missing dependencies and confused users

--- edrg.py
+++ edrg.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 edrg.py -- route planner for Rare Goods in Elite: Dangerous
 """
@@ -35,43 +34,43 @@ def session_scope():
 def initialize_db(wipe=False):
 	if wipe and os.path.isfile('edrg.sqlite'):
 		os.remove('edrg.sqlite')
-		
-	global engine 
-	global Session 
+
+	global engine
+	global Session
 	engine = create_engine('sqlite:///edrg.sqlite', echo=False)
 	Session = sessionmaker(bind=engine)
-	
+
 	if wipe:
 		Base.metadata.create_all(engine)
 		import_data_from_excel()
 
 class System(Base):
 	__tablename__ = 'system'
-	
+
 	id = Column(Integer, primary_key=True)
 	name = Column(String, unique=True, index=True)
 	x = Column(Float)
 	y = Column(Float)
 	z = Column(Float)
-	
+
 	stations = relationship("Station", backref="system")
-	
+
 #	@hybrid_method
 	def distanceTo(self, other):
 		return (((self.x - other.x) ** 2) + ((self.y - other.y) ** 2) + ((self.z - other.z) ** 2)) ** .5
-	
+
 #	@distanceTo.expression      # THIS DOESN'T WORK BECAUSE SQLITE DOESN'T HAVE THE POWER() FUNCTION
 #	def distanceTo(cls, other):
 #		return func.power(func.power(cls.x-other.x,2)+func.power(cls.y-other.y,2)+func.power(cls.z-other.z,2),.5)
-	
+
 	@classmethod
 	def import_(cls, sheet, session):
 		"""
 		Import the systems from the excel spreadsheet.
 		"""
-		# Many magic numbers here! By phrasing them as constants, hopefully it's at least easier to 
+		# Many magic numbers here! By phrasing them as constants, hopefully it's at least easier to
 		# modify when the sheet changes.
-		
+
 		FIRST_COLUMN = column('R')
 		LAST_COLUMN  = column('DU')
 		X_ROW        = 11
@@ -84,27 +83,27 @@ class System(Base):
 			x    = float(get_cell_value(sheet, (X_ROW, col), rcformat=True))
 			y    = float(get_cell_value(sheet, (Y_ROW, col), rcformat=True))
 			z    = float(get_cell_value(sheet, (Z_ROW, col), rcformat=True))
-			
+
 			System.add(session, name, x, y, z)
-		
+
 	@classmethod
 	def add(cls, session, name, x, y, z):
 		if session.query(System).filter_by(name=name).count() == 0:
 			session.add(System(name=name, x=x, y=y, z=z))
-		
-		
+
+
 class Station(Base):
 	__tablename__ = 'station'
-	
+
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
 	dist = Column(Float)
-	
+
 	system_id = Column(Integer, ForeignKey('system.id'))
 	goods = relationship("Goods", backref="station")
-	
+
 	Index('ix_name_system', 'name', 'system', unique=True)
-	
+
 	@classmethod
 	def import_(cls, sheet, session):
 		FIRST_ROW = 17
@@ -112,54 +111,54 @@ class Station(Base):
 		NAME_COL   = 'P'
 		SYSTEM_COL = 'Q'
 		DIST_COL   = 'M'
-		
+
 		for row in range(FIRST_ROW, LAST_ROW + 1): # add 1 for range behavior
 			name = strip(get_cell_value(sheet, NAME_COL   + str(row)))
 			sys  = strip(get_cell_value(sheet, SYSTEM_COL + str(row)))
 			dist = float(get_cell_value(sheet, DIST_COL   + str(row)))
-			
+
 			system = session.query(System).filter_by(name=sys).first()
-			
+
 			Station.add(session, name, system, dist)
-		
+
 	@classmethod
 	def add(cls, session, name, system, dist):
 		session.add(Station(name=name, dist=dist, system=system))
-	
+
 class Goods(Base):
 	__tablename__ = 'goods'
-	
+
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
 	max_cap = Column(Integer)
 	min_supply = Column(Integer)
 	max_supply = Column(Integer)
 	price = Column(Integer)
-	
+
 	station_id = Column(Integer, ForeignKey('station.id'))
-	
+
 	Index('ix_name_station', 'name', 'station', unique=True)
-	
+
 	@hybrid_property
 	def expected_supply(self):
 		return round((self.min_supply + self.max_supply) / 2)
-	
+
 	@expected_supply.expression
 	def expected_supply(cls):
 		return func.round((cls.min_supply + cls.max_supply) / 2)
-	
+
 	@hybrid_property
 	def min_value(self):
 		return self.price * self.min_supply
-		
+
 	@hybrid_property
 	def max_value(self):
 		return self.price * self.max_supply
-		
+
 	@hybrid_property
 	def expected_value(self):
 		return self.price * self.expected_supply
-		
+
 	@classmethod
 	def import_(cls, sheet, session):
 		FIRST_ROW = 17
@@ -172,13 +171,13 @@ class Goods(Base):
 		MIN_SUPPLY  = 'C'
 		MAX_SUPPLY  = 'D'
 		PRICE       = 'K'
-		
+
 		for row in range(FIRST_ROW, LAST_ROW + 1): # add 1 for range behavior
 			row = str(row)
 			name = strip(get_cell_value(sheet, NAME_COL    + row))
 			stt  = strip(get_cell_value(sheet, STATION_COL + row))
 			sys  = strip(get_cell_value(sheet, SYSTEM_COL  + row))
-			
+
 			mc = get_cell_value(sheet, MAX_CAP    + row)
 			if isinstance(mc, float):
 				max_cap = round(mc)
@@ -188,38 +187,38 @@ class Goods(Base):
 				max_cap = round(float(mc[:mc.index('-')]))
 			elif mc.endswith(' t'):
 				max_cap = round(float(mc[:-2]))
-					
+
 			if strip(get_cell_value(sheet, SUP_RATE + row)).upper() == 'ND':
 				min_supply = 0
 				max_supply = 0
 			else:
 				min_supply = round(float(get_cell_value(sheet, MIN_SUPPLY + row)))
 				max_supply = round(float(get_cell_value(sheet, MAX_SUPPLY + row)))
-				
+
 			price      = round(float(get_cell_value(sheet, PRICE      + row)))
-			
+
 			system  = session.query(System).filter_by(name=sys).first()
 			station = session.query(Station).filter_by(system=system, name=stt).first()
-			
+
 			Goods.add(session, station, name, max_cap, min_supply, max_supply, price)
-		
+
 	@classmethod
 	def add(cls, session, station, name, max_cap, min_supply, max_supply, price):
 		session.add(Goods(station=station, name=name, max_cap=max_cap, min_supply=min_supply, max_supply=max_supply, price=price))
-		
+
 	def __str__(self):
 		ev = self.expected_value
 		return "{} ({}, {}): {}".format(self.name, self.station.name, self.station.system.name, ev)
-	
+
 def import_data_from_excel(path="elite dangerous rare goods.xlsx"):
 	with xlrd.open_workbook(path) as book:
 		sheet = book.sheet_by_index(0)
-		
+
 		with session_scope() as session:
 			System.import_(sheet, session)
 			Station.import_(sheet, session)
 			Goods.import_(sheet, session)
-			
+
 def column(colname, exponent=0):
 	"Equivalent to the Excel COLUMN() command. Given 'A', returns 1 (case insensitive)"
 	if len(colname) == 0:
@@ -227,20 +226,20 @@ def column(colname, exponent=0):
 	colname = colname.lower()
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	return ((1 + alphabet.index(colname[-1])) * (26 ** exponent)) + column(colname[:-1], exponent+1)
-	
+
 def strip(str):
 	return str.strip() # forgot it was a method
-	
+
 def tquery(q):
 	"Convience method to get a scoped query"
 	with session_scope() as session:
 		return session.query(q)
-	
+
 def get_cell_value(sheet, address, rcformat=False):
 	"""
-	returns the value of the cell at the given address. 
-	
-	if rcformat is False, address is interpreted as a normal excel address: "H2" etc. 
+	returns the value of the cell at the given address.
+
+	if rcformat is False, address is interpreted as a normal excel address: "H2" etc.
 	if rcformat is True,  address is interpreted as a tuple (row, column)
 		The input addresses are as 1-indexed, as would be generated by Excel's ROW() and COLUMN() functions
 	"""
@@ -249,38 +248,38 @@ def get_cell_value(sheet, address, rcformat=False):
 	else:
 		address = address.lower()
 		match = re.match(r"\A(?P<col>[a-z]+)(?P<row>[0-9]+)\Z", address)
-		row =    int(match.group('row')) 
-		col = column(match.group('col')) 
+		row =    int(match.group('row'))
+		col = column(match.group('col'))
 	row -= 1 #xlrd indexes its rows from 0, and Excel from 1
 	col -= 1 #xlrd indexes its cols from 0, and Excel from 1
 	return sheet.cell_value(row, col)
-	
+
 def sale_price(good, destination, p1=16000, p2=0.0677, p3=101):
 	"""
 	The accepted formula for sale price of a rare good in this game:
-	
+
 	    SP(x) = p0 + p1 /  ( 1.0 + exp( - ( p2 * ( x – p3 ) ) ) )
-		
+
 	OK, so what are these numbers?
-	
+
 	Guessing here based on forum speculation:
 		p0 : purchase price / 2
-		p1 : variable per item; related to the maximal sale price. We don't know this, so we use 
-		     an estimate of 16000, which is close enough 
+		p1 : variable per item; related to the maximal sale price. We don't know this, so we use
+		     an estimate of 16000, which is close enough
 		p2 : galactic constant of approximately 0.0677
 		p3 : galactic constant of approximately 101
-		
+
 	ref: https://forums.frontier.co.uk/showthread.php?t=66538
 	"""
-	
+
 	dist = good.station.system.distanceTo(destination)
 	return (good.price / 2) + (p1 / (1.0 + exp( - (p2 * (dist - p3)))))
-	
+
 def optimize(goods, outputs=1, max_dist=None, max_cargo=None):
 	"""
 	Accepts a list of goods. Returns the pair with the highest round-trip profit.
 	"""
-	
+
 	results = []
 	while len(goods) > 0:
 		origin = goods.pop(0)
@@ -297,19 +296,19 @@ def optimize(goods, outputs=1, max_dist=None, max_cargo=None):
 				profit_back = des * (sale_price(destination, origin.station.system) - destination.price)
 				rt_profit = profit_out + profit_back
 				results.append((rt_profit, origin, destination, dist))
-	
+
 	results.sort(key=lambda ptuple: -ptuple[0]) #sorting by negative profit gives us the highest profits first
-	
+
 	for r in range(outputs):
 		profit, origin, destination, dist = results.pop(0)
 		print("Optimal route: "+ str(origin))
 		print(" and " + str(destination))
 		print(" at a distance of {:.2f} Ly with round-trip expected profit {:.0f}".format(dist, profit))
 		print()
-	
+
 if __name__ == '__main__':
 	import argparse
-	
+
 	parser = argparse.ArgumentParser(description="Work with the Elite Dangerous Rare Goods")
 	parser.add_argument('-w', '--wipe', action='store_true', default=False,
 		help="Wipe the database and recreate it from scratch from the spreadsheet")
@@ -329,44 +328,44 @@ if __name__ == '__main__':
 		help="Limit to the first N SQL results. This happens before optimization!")
 	parser.add_argument('-f', '--filter', action='append', default=[],
 		help="Raw SQLAlchemy filter strings. You have access to Goods, Station, and System.")
-	parser.add_argument('-s', '--sort', action='store', 
+	parser.add_argument('-s', '--sort', action='store',
 		help="Raw SQLAlchemy order_by string. Sorts descending by default. You have access to Goods, Station, and System.")
 	parser.add_argument('-a', '--ascending', action='store_true', default=False,
 		help="Modifies --sort to produce an ascending sort instead.")
-	
-		
+
+
 	ns = parser.parse_args()
-	
+
 	if not any([ns.wipe, ns.count, ns.display, ns.optimize]):
 		parser.print_help()
 	else:
 		initialize_db(ns.wipe)
 		with session_scope() as session:
 			q = session.query(Goods).join(Station).join(System)
-			
+
 			# filters need to be eval'd for hybrid properties to work. Do it safely, though:
 			gbs = {'__builtins__':None}
 			lcs = {'Goods':Goods, 'Station':Station, 'System':System}
 			for filter in ns.filter:
 				q = q.filter(eval(filter, gbs, lcs))
-			
+
 			if ns.count:
 				print(q.count())
 				print()
-			
+
 			if ns.sort is not None:
 				order = asc if ns.ascending else desc
 				q = q.order_by(order(eval(ns.sort, gbs, lcs)))
-				
+
 			if ns.limit != -1:
 				q = q.limit(ns.limit)
-			
+
 			if ns.display:
 				for result in q.all():
 					print(str(result))
 				print()
-				
+
 			if ns.optimize:
 				optimize(q.all(), ns.optimize_outputs, ns.max_dist, ns.limit_cargo)
-	
+
 	#TODO: work on the optimization logic, which after all was the whole point
